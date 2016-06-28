ce7db23bd7197c46bcc5108087484678a927e82d
metraf <firsov.tyoma@gmail.com>
2016-06-28 15:15:25 +0200

    old code

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,49 @@
+var i=2;
 $(document).ready(function() {
-	$('button').click(function() {
-	$('tbody').append('<tr><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>');
+	$('#button').click(function() {
+		var cop = $('#tCop').clone();
+		cop.attr('id', 'tCop'+i);
+		//cop.find('#number1').attr('id', 'number'+i).text(i+".");
+		cop.find('#product1').attr('id', 'product'+i).val('');
+		cop.find('#weight1').attr('id', 'weight'+i).val('');
+		cop.find('#protein1').attr('id', 'protein'+i);
+		cop.find('#fatt1').attr('id', 'fatt' +i);
+		cop.find('#carbonates1').attr('id', 'carbonates'+i);
+		cop.find('#kcal1').attr('id', 'kcal'+i);
+		cop.append('<th><a></a></th>').find('a').attr('id', i).text('Löschen');
+		cop.appendTo('tbody');
+		i++;
+		delEl();
+		modalAdd();
+		autoCom();
 	});
 });
+function delEl(){
+	$('a').click(function() {
+		var el_id = $(this).attr('id');
+		$('#tCop'+el_id).remove();
+		//alert('tCop'+el_id);
+	});
+}
+function modalAdd(){
+	$('#addNeu').click(function() {
+		$('#modal').modal();
+	        return false;
+	});
+}
+/*function autoCom(){
+	var data;
+	$.ajax({
+	url: 'content.php',
+	type: 'POST',
+	success: function(html){
+	data = html;
+	}
+	});
+					alert(data);
+					
+                    /*$("#product1").autocomplete({
+                        source:'content.php',
+                        minLength:1
+                    });*/
+//}
