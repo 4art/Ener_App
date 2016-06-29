5cd86a8501d00050aa04ff983ac84b97becb4069
metraf <firsov.tyoma@gmail.com>
2016-06-29 23:53:55 +0200

    del

--- js/ang.js
+++ js/ang.js
@@ -6,47 +6,11 @@ var app=/**
 angular.module('app', []);
 
 app.controller('mainCtrl', function($scope){
-	$scope.itemsList = [
-    {'name': 'Iapetos Baltazar', 'amount': 1},
-    {'name': 'Mordred Maui', 'amount': 4},
-    {'name': 'Verethragna Tane', 'amount': 20},
-    {'name': 'Arawn Lir', 'amount': 7},
-    {'name': 'Cepheus Zephyrus', 'amount': 15},
-    {'name': 'Leander Pallas', 'amount': 9},
-    {'name': 'Chryses Vohu Manah', 'amount': 13},
-    {'name': 'Nuadha Lycus', 'amount': 19}
-  ];
+	
 });
-/*app.directive('dropdownList',function( $timeout ){
-  return {
-    restrict: 'E',
-    scope: {
-      itemsList: '=',
-      placeholder: '@'
-    },
-    template: '<input type="text" ng-model="search" placeholder="{{ placeholder }}" />' +
-				'<div class="search-item-list"><ul class="list">' +
-				'<li ng-repeat="item in itemsList | filter:search" ng-click="chooseItem( item )">{{ item.name }}' +
-          '<span class="amount">{{ item.amount }}</span>' +
-    		 '</li>' +
-				'</ul></div>',
-    link: function(scope, el, attr){
-        var $listContainer = angular.element( el[0].querySelectorAll('.search-item-list')[0] );
-        el.find('input').bind('focus',function(){
-          $listContainer.addClass('show');
-        });
-        el.find('input').bind('blur',function(){
-          /*
-             * 'blur' реагирует быстрее чем ng-click,
-             * поэтому без $timeout chooseItem не успеет поймать item до того, как лист исчезнет
-             */
-          /*$timeout(function(){ $listContainer.removeClass('show') }, 200);
-        });
-      
-      	scope.chooseItem = function( item ){
-				 scope.search = item.name;
-           $listContainer.removeClass('show');
-			 }
-    }
-  }
-});*/
+$scope.products = [
+    {'name': 'hähnchen', 'protein': 0.207, 'fat': 0.085, 'carbo': 0.004, 'kcal':1.99},
+    {'name': 'putenfleisch', 'protein': 0.213, 'fat': 0.121, 'carbo': 0.008, 'kcal':1.98},
+    {'name': 'gänsefleisch', 'protein': 0.292, 'fat': 0.222, 'carbo': 0.00, 'kcal':3.19},
+    
+  ];
