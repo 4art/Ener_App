511589f58678549a9791ca2131f1f6a7b5f55aed
metraf <firsov.tyoma@gmail.com>
2016-07-01 00:39:45 +0200

    add cout logic without general

--- js/logic.js
+++ js/logic.js
@@ -8,12 +8,16 @@ jquery:
 	ui to name with auto insert to hidden
 */
 
-
 var i=2;
+var products = [
+    {'label': 'hähnchen', 'protein': 0.207, 'fat': 0.085, 'carbo': 0.004, 'kcal':1.99},
+    {'label': 'putenfleisch', 'protein': 0.213, 'fat': 0.121, 'carbo': 0.008, 'kcal':1.98},
+    {'label': 'gänsefleisch', 'protein': 0.292, 'fat': 0.222, 'carbo': 0.00, 'kcal':3.19},
+    
+  ];
 $(document).ready(function() {
 		autoCom();
-		$('#hiddenP1').val(0);
-		$('#weight1').val(0);
+		prodCount();
 	$('#button').click(function() {
 		var cop = $('#tCop').clone();
 		if ($('#tCop'+(i-1)).find('#product'+(i-1)).val()==""||$('#tCop').find('#product'+(i-1)).val()=="") {
@@ -24,10 +28,10 @@ $(document).ready(function() {
 		cop.find('#number1').attr('id', 'number'+i).text(i+".");
 		cop.find('#product1').attr('id', 'product'+i).val('');
 		cop.find('#weight1').attr('ng-model', 'weight'+i).attr('id', 'weight'+i).val('');
-		cop.find('#protein1').attr('id', 'protein'+i);
-		cop.find('#fat1').attr('id', 'fat' +i);
-		cop.find('#carbonates1').attr('id', 'carbonates'+i);
-		cop.find('#kcal1').attr('id', 'kcal'+i);
+		cop.find('#protein1').attr('id', 'protein'+i).empty();
+		cop.find('#fat1').attr('id', 'fat' +i).empty();
+		cop.find('#carbonates1').attr('id', 'carbonates'+i).empty();
+		cop.find('#kcal1').attr('id', 'kcal'+i).empty();
 		cop.find('#hiddenP1').attr('ng-model', 'protein'+i).attr('id', 'hiddenP'+i);
 		cop.find('#hiddenF1').attr('ng-model', 'fat'+i).attr('id', 'hiddenF'+i);
 		cop.find('#hiddenC1').attr('ng-model', 'carbo'+i).attr('id', 'hiddenC'+i);
@@ -36,6 +40,7 @@ $(document).ready(function() {
 		cop.appendTo('tbody');
 		i++;
 		delEl();
+		prodCount();
 		modalAdd();
 		autoCom();
 
@@ -70,21 +75,24 @@ function autoCom(){
                         minLength:1
                     });*/
 
-var products = [
-    {'label': 'hähnchen', 'protein': 0.207, 'fat': 0.085, 'carbo': 0.004, 'kcal':1.99},
-    {'label': 'putenfleisch', 'protein': 0.213, 'fat': 0.121, 'carbo': 0.008, 'kcal':1.98},
-    {'label': 'gänsefleisch', 'protein': 0.292, 'fat': 0.222, 'carbo': 0.00, 'kcal':3.19},
-    
-  ];
+
             $( "#product"+(i-1) ).autocomplete({
-               source: products,
-               select: function(event, ui){
-                  $('#hiddenP'+(i-1)).val(ui.item.protein);
-                  $('#hiddenF'+(i-1)).val(ui.item.fat);
-                  $('#hiddenC'+(i-1)).val(ui.item.carbo);
-                  $('#hiddenK'+(i-1)).val(ui.item.kcal);
+               	source: products,
+               	select: function(event, ui){
+                	$('#hiddenP'+(i-1)).val(ui.item.protein);
+                	$('#hiddenF'+(i-1)).val(ui.item.fat);
+                	$('#hiddenC'+(i-1)).val(ui.item.carbo);
+                	$('#hiddenK'+(i-1)).val(ui.item.kcal);
                  
                }
             });
 
+}
+function prodCount(){
+	$('#weight'+(i-1)).change(function() {
+			$('#protein'+(i-1)).html($(this).val()*$('#hiddenP'+(i-1)).val());
+			$('#fat'+(i-1)).html($(this).val()*$('#hiddenF'+(i-1)).val());
+			$('#carbonates'+(i-1)).html($(this).val()*$('#hiddenC'+(i-1)).val());
+			$('#kcal'+(i-1)).html($(this).val()*$('#hiddenK'+(i-1)).val());
+		});
 }
