c10eb29486aa30b4d33169f6d461c63f71eb41b5
metraf <firsov.tyoma@gmail.com>
2016-07-05 23:45:30 +0200

    need to add fonts

---
+++ includes/meta.php
@@ -0,0 +1,12 @@
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link href="css/carousel.css" rel="stylesheet">
+  <script src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/logic.js"></script>
+  <script type="text/javascript" src="js/angular.min.js"></script>
+  <script src="js/holder.min.js"></script>
