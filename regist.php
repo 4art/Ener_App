6ece8b9948649a9f6afeca63ffda232074dfced0
metraf <firsov.tyoma@gmail.com>
2016-07-20 22:29:56 +0200

    added ids for regist

--- regist.php
+++ regist.php
@@ -13,19 +13,19 @@
        <!-- <h2 class="form-signin-heading">Bitte Loggen Sie sich ein.</h2>-->
         <div class="form-group">
           <label for="inputName" class="sr-only">Name</label>
-          <input type="test" id="inputEmail_Log" class="form-control" placeholder="Name" required autofocus>
+          <input type="test" id="inputName_Reg" class="form-control" placeholder="Name" required autofocus>
         </div>
         <div class="form-group">
           <label for="inputEmail" class="sr-only">Email</label>
-          <input type="email" id="inputEmail_Log" class="form-control" placeholder="Email" required autofocus>
+          <input type="email" id="inputEmail_Reg" class="form-control" placeholder="Email" required autofocus>
         </div>
         <div class="form-group">
           <label for="inputPassword" class="sr-only">Kennword</label>
-          <input type="password" id="inputPassword_Log" class="form-control" placeholder="Kennword" required>
+          <input type="password" id="inputPassword_Reg" class="form-control" placeholder="Kennword" required>
         </div>
         <div class="form-group">
           <label for="inputPassword" class="sr-only">Kennword wiederholen</label>
-          <input type="password" id="inputPassword_Log" class="form-control" placeholder="Kennword wieder" required>
+          <input type="password" id="inputPassword_Reg" class="form-control" placeholder="Kennword wieder" required>
         </div>
         <button id="reg_but" class="btn btn-lg btn-primary btn-block" type="submit">Registrieren</button>
       </form>
